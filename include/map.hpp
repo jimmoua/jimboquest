b3655f9093601b976f30d8352e7ef5df4b1b6878
Jim Moua <jmoua@protonmail.com>
2019-04-11 23:20:22 -0500

    Added a way to read from the map txt files. Have not tested anything, but everything compiles without warnings

--- include/map.hpp
+++ include/map.hpp
@@ -3,6 +3,9 @@
 #include <SFML/Audio.hpp>
 #include "window.hpp"
 #include "game.hpp"
+#include <string>
+#include <vector>
+#include <fstream>
 
 /* For maps, I will be using four layers. These layers will correspond to the
  * following:
@@ -23,18 +26,18 @@
  * When we have a texture and we set a sprite to that texture, we can expect it
  * to look something like this (if tiling):
  *
- *                               SPRITE LAYOUT
- *                               -------------
- *                        0  1  2  3  4  5  6  7  8  9
- *                       10 11 12 13 14 15 16 17 18 19
- *                       20 21 22 23 24 25 26 27 28 29
- *                       30 31 32 33 34 35 36 37 38 39
- *                       40 41 42 43 44 45 46 47 48 49
- *                       50 51 52 53 54 55 56 57 58 59
- *                       60 61 62 63 64 65 66 67 68 69
- *                       70 71 72 73 74 75 76 77 78 79
- *                       80 81 82 83 84 85 86 87 88 89
- *                       90 91 92 93 94 95 96 97 98 99
+ *   SPRITE LAYOUT
+ *   -------------
+ *    0  1  2  3  4  5  6  7  8  9
+ *   10 11 12 13 14 15 16 17 18 19
+ *   20 21 22 23 24 25 26 27 28 29
+ *   30 31 32 33 34 35 36 37 38 39
+ *   40 41 42 43 44 45 46 47 48 49
+ *   50 51 52 53 54 55 56 57 58 59
+ *   60 61 62 63 64 65 66 67 68 69
+ *   70 71 72 73 74 75 76 77 78 79
+ *   80 81 82 83 84 85 86 87 88 89
+ *   90 91 92 93 94 95 96 97 98 99
  *
  * For example:
  *   0 = Nothing
@@ -48,57 +51,75 @@
  * Our map will look something like this:
  *
  *
- *  10 10                               Tells us that the map is a 10x10
+ * Tells us that the map is a 10x10 map
+ * 10 10
  *
- *   0  1  2  3  4  5  6  7  8  9       LAYER 1 = Walkable layer
- *  10 11 12 13 14 15 16 17 18 19
- *  20 21 22 23 24 25 26 27 28 29
- *  30 31 32 33 34 35 36 37 38 39
- *  40 41 42 43 44 45 46 47 48 49
- *  50 51 52 53 54 55 56 57 58 59
- *  60 61 62 63 64 65 66 67 68 69
- *  70 71 72 73 74 75 76 77 78 79
- *  80 81 82 83 84 85 86 87 88 89
- *  90 91 92 93 94 95 96 97 98 99
+ *    LAYER 1 = Walkable layer
+ *    0  1  2  3  4  5  6  7  8  9
+ *   10 11 12 13 14 15 16 17 18 19
+ *   20 21 22 23 24 25 26 27 28 29
+ *   30 31 32 33 34 35 36 37 38 39
+ *   40 41 42 43 44 45 46 47 48 49
+ *   50 51 52 53 54 55 56 57 58 59
+ *   60 61 62 63 64 65 66 67 68 69
+ *   70 71 72 73 74 75 76 77 78 79
+ *   80 81 82 83 84 85 86 87 88 89
+ *   90 91 92 93 94 95 96 97 98 99
  *
- *   0  1  2  3  4  5  6  7  8  9       Layer 2 = Collision layer
- *  10 11 12 13 14 15 16 17 18 19
- *  20 21 22 23 24 25 26 27 28 29
- *  30 31 32 33 34 35 36 37 38 39
- *  40 41 42 43 44 45 46 47 48 49
- *  50 51 52 53 54 55 56 57 58 59
- *  60 61 62 63 64 65 66 67 68 69
- *  70 71 72 73 74 75 76 77 78 79
- *  80 81 82 83 84 85 86 87 88 89
- *  90 91 92 93 94 95 96 97 98 99
+ *   Layer 2 = Collision layer
+ *    0  1  2  3  4  5  6  7  8  9
+ *   10 11 12 13 14 15 16 17 18 19
+ *   20 21 22 23 24 25 26 27 28 29
+ *   30 31 32 33 34 35 36 37 38 39
+ *   40 41 42 43 44 45 46 47 48 49
+ *   50 51 52 53 54 55 56 57 58 59
+ *   60 61 62 63 64 65 66 67 68 69
+ *   70 71 72 73 74 75 76 77 78 79
+ *   80 81 82 83 84 85 86 87 88 89
+ *   90 91 92 93 94 95 96 97 98 99
  *
- *   0  1  2  3  4  5  6  7  8  9       Layer 3
- *  10 11 12 13 14 15 16 17 18 19
- *  20 21 22 23 24 25 26 27 28 29
- *  30 31 32 33 34 35 36 37 38 39
- *  40 41 42 43 44 45 46 47 48 49
- *  50 51 52 53 54 55 56 57 58 59
- *  60 61 62 63 64 65 66 67 68 69
- *  70 71 72 73 74 75 76 77 78 79
- *  80 81 82 83 84 85 86 87 88 89
- *  90 91 92 93 94 95 96 97 98 99
+ *   Layer 3 = Passthrough layer
+ *    0  1  2  3  4  5  6  7  8  9
+ *   10 11 12 13 14 15 16 17 18 19
+ *   20 21 22 23 24 25 26 27 28 29
+ *   30 31 32 33 34 35 36 37 38 39
+ *   40 41 42 43 44 45 46 47 48 49
+ *   50 51 52 53 54 55 56 57 58 59
+ *   60 61 62 63 64 65 66 67 68 69
+ *   70 71 72 73 74 75 76 77 78 79
+ *   80 81 82 83 84 85 86 87 88 89
+ *   90 91 92 93 94 95 96 97 98 99
  *
- *   0  1  2  3  4  5  6  7  8  9       Layer 4 = Event layer
- *  10 11 12 13 14 15 16 17 18 19
- *  20 21 22 23 24 25 26 27 28 29
- *  30 31 32 33 34 35 36 37 38 39
- *  40 41 42 43 44 45 46 47 48 49
- *  50 51 52 53 54 55 56 57 58 59
- *  60 61 62 63 64 65 66 67 68 69
- *  70 71 72 73 74 75 76 77 78 79
- *  80 81 82 83 84 85 86 87 88 89
- *  90 91 92 93 94 95 96 97 98 99
+ *   Layer 4 = Event layer
+ *    0  1  2  3  4  5  6  7  8  9
+ *   10 11 12 13 14 15 16 17 18 19
+ *   20 21 22 23 24 25 26 27 28 29
+ *   30 31 32 33 34 35 36 37 38 39
+ *   40 41 42 43 44 45 46 47 48 49
+ *   50 51 52 53 54 55 56 57 58 59
+ *   60 61 62 63 64 65 66 67 68 69
+ *   70 71 72 73 74 75 76 77 78 79
+ *   80 81 82 83 84 85 86 87 88 89
+ *   90 91 92 93 94 95 96 97 98 99
  *
  *
  * Since there can only be one map loaded at a time, I have decided to just go
  * with encapsulation again instead of classes. */
 
-namespace {
+/* In order to have our map display an image, we need to have two things:
+ *   1. Texture -> Some piece in memory to hold the graphics
+ *   2. Sprite  -> Some rectangle to hold the texture
+ * 
+ * We will create the texture in the assets file with sf::Texture and load it
+ * with a texture from file, memory, or from stream (preferrably from file).
+ * 
+ * Once we have created a texture, we will assign a sprite to it. Textures are
+ * very expensive, but sprites are not. For this reason, using multiple sprites
+ * in a map seems OK to me.
+ * */
+
+namespace game {
   namespace map {
+    void loadMap(const std::string&);
   }
 }
--- src/map.cc
+++ src/map.cc
@@ -1,4 +1,56 @@
 #include "map.hpp"
 
 namespace {
+  static std::vector< std::vector<size_t> > _map_lay01;  // Layer 1
+  static std::vector< std::vector<size_t> > _map_lay02;  // Layer 2
+  static std::vector< std::vector<size_t> > _map_lay03;  // Layer 3
+  static std::vector< std::vector<size_t> > _map_EvLay;  // Event layer
+}
+
+void game::map::loadMap(const std::string& mapName) {
+
+  std::ifstream infile(mapName);
+  if(infile) {
+    std::cerr << "Unable to open map: " << mapName << std::endl;
+    std::cerr << "Verify the integrity of " << mapName << std::endl;
+    exit(1);
+  }
+  unsigned short int row, col;
+  /* The first two numbers in the map file are always row and col */
+  infile >> row >> col;
+
+  /* Read in the first layer */
+  for(size_t i = 0; i < row; i++) {
+    for(size_t j = 0; j < col; j++) {
+      infile >> _map_lay01[i][j];
+    }
+  }
+
+  /* Read in the second layer */
+  for(size_t i = 0; i < row; i++) {
+    for(size_t j = 0; j < col; j++) {
+      infile >> _map_lay02[i][j];
+    }
+  }
+
+  /* Read in the third layer */
+  for(size_t i = 0; i < row; i++) {
+    for(size_t j = 0; j < col; j++) {
+      infile >> _map_lay03[i][j];
+    }
+  }
+
+  /* Read in the event layer */
+  for(size_t i = 0; i < row; i++) {
+    for(size_t j = 0; j < col; j++) {
+      infile >> _map_EvLay[i][j];
+    }
+  }
+
+  if(infile.peek() == '\0') {
+    std::cerr << "Error: detected extra bits at the end of the map\n";
+    infile.close();
+    exit(1);
+  }
+
 }
