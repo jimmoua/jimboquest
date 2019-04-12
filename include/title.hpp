c14e63bb871e2f8e4a6f6a9e0e4ebe2fb51752e2
Jim Moua <jmoua@protonmail.com>
2019-04-11 21:10:21 -0500

    Added title.hpp and defined an init function

---
+++ include/title.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "asset.hpp"
+#include "window.hpp"
+#include "game.hpp"
+
+namespace game {
+  namespace title {
+    void init();
+  }
+}
