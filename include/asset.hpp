0cd5ef86ae99edba0b08bc7fd5e345f83ee770d8
Jim Moua <jmoua@protonmail.com>
2019-04-11 21:15:07 -0500

    Added asset namespace in asset.hpp and defined some nice sfml text creatining functions.

--- include/asset.hpp
+++ include/asset.hpp
@@ -1,5 +1,33 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <map>
+#include <string>
+
+using ushort = unsigned short int;
 
 namespace game {
+  namespace asset {
+    /* GS = Game states */
+    enum class GS {
+      TITLE,
+      NONE // for when exit
+    };
+
+    /* use these for std::map for img path */
+    enum class img {
+      TS_BG
+    };
+
+    void init();
+
+    sf::Text createString(const std::string,  // string name
+                          const ushort,       // text size
+                          const sf::Color,    // fill color
+                          const sf::Color,    // outline color
+                          const float);       // outline thickness
+
+    void setTxtOriginCenter(sf::Text&);
+  }
 }
