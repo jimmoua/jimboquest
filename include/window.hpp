7b42325fa00469ee3ce4422798e385c598048323
Jim Moua <jmoua@protonmail.com>
2019-04-11 21:11:04 -0500

    Changed the renderwindow from pointer data type to normal data type.

--- include/window.hpp
+++ include/window.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 namespace game {
   namespace win {
     void init();
-    sf::RenderWindow*& getWin();
+    sf::RenderWindow& getWin();
     sf::Event& getEv();
     const unsigned short int& getRes_x();
     const unsigned short int& getRes_y();
