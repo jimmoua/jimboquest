cccccbe234d6db0bbc056137f0a7c1de397b8d9a
Jim Moua <jmoua@protonmail.com>
2019-04-29 02:04:49 -0500

    join all threads before closing program

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,12 @@
 int main() {
   game::init();
   game::run();
+  /* If we have threads going on, wait for them before closing the game */
+  for(auto& i : game::thread_vector()) {
+    if(i.joinable()) {
+      i.join();
+    }
+  }
   game::asset::MusicClean();
   return 0;
 }
