34e723f6aac19df3f454f400008a32d269f1fc8d
Jim Moua <jmoua@protonmail.com>
2019-05-08 11:58:57 -0500

    Added a particles animation, not tested yet

--- include/animations.hpp
+++ include/animations.hpp
@@ -6,6 +6,7 @@ namespace game {
 
   namespace ani {
     void fadeOut();
+    void particleEffect();
   }
 
 }
--- src/animations.cc
+++ src/animations.cc
@@ -5,6 +5,7 @@
 #include "map.hpp"
 #include "entity.hpp"
 #include "animations.hpp"
+#include <random>
 
 namespace {
   static sf::RectangleShape r;
@@ -29,3 +30,68 @@ void game::ani::fadeOut() {
   r.setFillColor(sf::Color(0,0,0,0));
 }
 
+void game::ani::particleEffect() {
+  /* This function is going to generate a particle effect. The reason for this
+   * function is because I want to have some particles floating about in the
+   * title screen to make it look less boring */
+
+  /* Lambda that will create particles */
+  auto createParticle = []() -> sf::CircleShape {
+    sf::CircleShape c;
+    c.setFillColor(sf::Color::Yellow);
+    c.setRadius(2.0f);
+    /* Place the particles at the bottom right of the screen. We will then move
+     * in directions to simulate a float effect */
+    c.setPosition(win::getWin().getSize().x, win::getWin().getSize().y);
+    return c;
+  };
+
+  static std::vector<sf::CircleShape> particles;
+  constexpr size_t maxParticles = 500;
+  if(particles.capacity() != maxParticles) {
+    particles.resize(maxParticles);
+  }
+  if(particles.size() != maxParticles) {
+    for(size_t i = particles.size(); i < maxParticles; i++) {
+      particles.push_back(createParticle());
+    }
+  }
+
+  /* 1 = up left
+   * 2 = straight up
+   * 3 = up right */
+  
+  /* Move the particles */
+  for(auto& iter : particles) {
+    static std::random_device rd;
+    std::mt19937 seed(rd());
+    std::uniform_int_distribution<> rand(1, 2);
+    auto dir = rand(seed);
+    if(dir == 1) {
+      iter.move(-1,-1);
+    }
+    else if(dir == 2) {
+      iter.move(0, -.5);
+    }
+    else if(dir == 3) {
+      iter.move(1, -1);
+    }
+  }
+
+  for(auto iter = particles.begin(); iter!= particles.end(); iter++) {
+    #define pos iter->getPosition()
+    if(pos.x < 0 || pos.x > win::getRes_x() || pos.y < 0 || pos.y > win::getRes_y()) {
+      particles.erase(iter);
+    }
+    #undef pos
+  }
+
+  /* Draw the particles on the screen */
+  for(auto& i : particles) {
+    win::getWin().draw(i);
+  }
+
+
+  /* After generating the particles, we will draw them */
+  win::display();
+}
