09ffdbd27c0ec38ff5948c141313ce2c84772b99
Jim Moua <jimmoua24@gmail.com>
2019-10-23 22:57:44 -0500

    Fixed some formatting and some coments

--- src/battle.cc
+++ src/battle.cc
@@ -82,60 +82,63 @@ void game::initBattle() {
     i.move(0, 220);
   }
 
-   // Players will fight monsters depending on what their levels are.
-   if(game::entity::getPl().f_getLevel() < 5) {
-     /* EASY LEVELED MONSTERS */
-     _createSlimeGroup_3();
-   }
+  ////////////////////////////////////////////////// 
+  // Generate the monsters to fight based on the
+  // player's level
+  ////////////////////////////////////////////////// 
+  if(game::entity::getPl().f_getLevel() < 5) {
+    /* EASY LEVELED MONSTERS */
+    _createSlimeGroup_3();
+  }
 
-   /* While there are monsters, keep the fight going. The condition will be
-    * something like 
-    *
-    *   while(!monsters.empty())
-    *     ...
-    *
-    * */
+  /* While there are monsters, keep the fight going. The condition will be
+   * something like 
+   *
+   *   while(!monsters.empty())
+   *     ...
+   *
+   * */
 
-   /* Get the view for the window so we can use it when displaying monsters. We
-    * will then need an offset, since l_battleWindow is not the same as as the
-    * view size */
+  /* Get the view for the window so we can use it when displaying monsters. We
+   * will then need an offset, since l_battleWindow is not the same as as the
+   * view size */
 
-   // offset is going to be the amount of times we will move each sprite for
-   // every monster we have in the list. offset is important because it will be
-   // the spacing between each monster.
-   auto offset = l_battleWindow[1].getSize().x/(_battleData::_monsterSprites.size()+1);
+  // offset is going to be the amount of times we will move each sprite for
+  // every monster we have in the list. offset is important because it will be
+  // the spacing between each monster.
+  auto offset = l_battleWindow[1].getSize().x/(_battleData::_monsterSprites.size()+1);
 
-   // pos is going to be the position of battleWindow[1].
-   auto pos = l_battleWindow[1].getPosition(); 
+  // pos is going to be the position of battleWindow[1].
+  auto pos = l_battleWindow[1].getPosition(); 
 
-   // Set the initial position to the right of the battle window. We will then
-   // add pos.x to it. We do this so there is the correct amount of spacing
-   // between the monsters and the window.
-   auto initPos = pos.x + offset - l_battleWindow[1].getSize().x/2; 
+  // Set the initial position to the right of the battle window. We will then
+  // add pos.x to it. We do this so there is the correct amount of spacing
+  // between the monsters and the window.
+  auto initPos = pos.x + offset - l_battleWindow[1].getSize().x/2; 
 
-   // Set the monster locations on the screen
-   for(unsigned int i = 0; i < _battleData::_monsterSprites.size(); i++) {
-     _battleData::_monsterSprites[i].setPosition(initPos + offset*(i), pos.y);
-   }
-   printf("The size is: ");
-   std::cout << _battleData::_monsterSprites.size() << std::endl;
+  // Set the monster locations on the screen
+  for(unsigned int i = 0; i < _battleData::_monsterSprites.size(); i++) {
+    _battleData::_monsterSprites[i].setPosition(initPos + offset*(i), pos.y);
+  }
+  printf("The size is: ");
+  std::cout << _battleData::_monsterSprites.size() << std::endl;
 
-   while(true) {
-     while(game::win::getWin().pollEvent(game::win::getEv())) {
-       if(game::win::getEv().key.code == sf::Keyboard::Escape) {
-         return;
-       }
-     }
-     game::win::getWin().draw(l_battleWindow[0]);
-     game::win::getWin().draw(l_battleWindow[1]);
-     game::win::getWin().draw(l_battleWindow_Choices[0]);
-     game::win::getWin().draw(l_battleWindow_Choices[1]);
-     /* Set the monster positions relative to the window */
-     for(auto& i : _battleData::_monsterSprites) {
-       game::win::getWin().draw(i);
-     }
-     game::win::getWin().display();
-   }
+  while(true) {
+    while(game::win::getWin().pollEvent(game::win::getEv())) {
+      if(game::win::getEv().key.code == sf::Keyboard::Escape) {
+        return;
+      }
+    }
+    game::win::getWin().draw(l_battleWindow[0]);
+    game::win::getWin().draw(l_battleWindow[1]);
+    game::win::getWin().draw(l_battleWindow_Choices[0]);
+    game::win::getWin().draw(l_battleWindow_Choices[1]);
+    /* Set the monster positions relative to the window */
+    for(auto& i : _battleData::_monsterSprites) {
+      game::win::getWin().draw(i);
+    }
+    game::win::getWin().display();
+  }
 
 
   /* end of battle */
